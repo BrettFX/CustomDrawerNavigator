09b2133c014dedf7707630a333a06b1a6be14934
Brett Allen <brettallen777@gmail.com>
2020-01-23 21:31:23 -0500

    Finish tutorial and implement basic app drawer example with hamburger context menu

--- App.js
+++ App.js
@@ -4,20 +4,38 @@ Left off at (1:43): https://youtu.be/7uhJN4kVS6g?t=103
 */
 
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { createDrawerNavigator } from 'react-navigation';
+import { StyleSheet, Text, View, SafeAreaView, ScrollView, Dimensions, Image } from 'react-native';
+import { createDrawerNavigator, DrawerItems } from 'react-navigation';
+import HomeScreen from './screens/HomeScreen';
+import SettingsScreen from './screens/SettingsScreen';
 
+const { width } = Dimensions.get('window');
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <AppDrawerNavigator/>
   );
 }
+
+const CustomDrawerComponent = (props) => (
+  <SafeAreaView style={{flex: 1}}>
+    <View style={{height: 150, backgroundColor: 'white', alignItems: 'center', justifyContent: 'center'}}>
+      <Image source={require('./assets/react-native.png')} style={{height:120, width:120, borderRadius: 60}} />
+    </View>
+    <ScrollView>
+      <DrawerItems {...props} />
+    </ScrollView>
+  </SafeAreaView>
+);
 
 const AppDrawerNavigator = createDrawerNavigator({
   Home: HomeScreen,
   Settings: SettingsScreen
+},{
+  contentComponent: CustomDrawerComponent,
+  drawerWidth: width,
+  contentOptions: {
+    activeTintColor: 'orange'
+  }
 });
 
 const styles = StyleSheet.create({
